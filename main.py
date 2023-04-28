2264e0e33697e5bad3cae66ef9f309929656b756
Martin Binamé <marti>
2023-04-28 22:14:14 +0200

    Introduction to widget label

--- main.py
+++ main.py
@@ -8,7 +8,11 @@ class MyApp:
 
         root.geometry("500x400")
         root.maxsize(1000,800)
+        self.initUI(root)
     
+    def initUI(self,root):
+        label = Label(root, text="Some label text")
+        label.pack()
 
 root =Tk()
 
